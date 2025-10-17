ce9b595dd6fecb22faa6acd99b28f1da3382bb70
gianluca-merendino <gmerendino@alumnos.exa.unicen.edu.ar>
2025-10-17 19:14:37 -0300

    corregidos links

--- js/indexJS.js
+++ js/indexJS.js
@@ -366,5 +366,5 @@ const logoutButton = document.querySelector('.logout');
     logoutButton.addEventListener('click', () => {
         
         console.log('Cerrando sesión y redirigiendo...');
-        window.location.href = '/tp-interfaces-g25/sing-in.html'; 
+        window.location.href = './sing-in.html'; 
     });
