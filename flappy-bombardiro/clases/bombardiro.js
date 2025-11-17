39c47f2b194bfdc0a0ea49f564a8598b98d78975
lupomanuel43 <lupomanuel43@gmail.com>
2025-11-17 18:51:14 -0300

    [flappy]: Comenzada clase bombardiro, agregada clase para representar el fondo

---
+++ flappy-bombardiro/clases/bombardiro.js
@@ -0,0 +1,9 @@
+class Bombardiro {
+    /**
+     * 
+     * @param {HTMLElement} element Elemento del DOM que representa a bombardiro
+     */
+    constructor(element) {
+        this.DOMElement = element
+    }
+}
