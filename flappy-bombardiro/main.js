4346b71e91637a3dccd13fd0769027ba4008f696
Virginia <virginiasr19@gmail.com>
2025-11-27 16:29:39 -0300

    local changes

--- flappy-bombardiro/main.js
+++ flappy-bombardiro/main.js
@@ -2,7 +2,4 @@ import { Game } from './clases/game.js';
 
 // --- Inicialización ---
 const bombardiroGame = new Game();
-document.getElementById("btn-play").addEventListener('click', ()=>{
-    console.log("Iniciando juego Bombardiro");
-    bombardiroGame.init();
-})
+bombardiroGame.init(); 
